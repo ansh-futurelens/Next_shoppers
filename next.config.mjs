79a899d5ecbb0a84f701517101196a4380a2c7ff
ansh-futurelens <futurelensdeveloper8@gmail.com>
2024-04-24 14:39:42 +0530

    SP-9:remove unnnessessory env from config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    NODE_ENV : process.env.NODE_ENV,
     DEV_DATABASE_URL: process.env.DEV_DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     PROD_DATABASE_URL: process.env.PROD_DATABASE_URL,
