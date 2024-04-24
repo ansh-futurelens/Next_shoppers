a90e8fc65ca8a9d350e4c498a15c4c3e19a7a881
ansh-futurelens <futurelensdeveloper8@gmail.com>
2024-04-24 12:15:20 +0530

    SP-2:Fix build verify-email file issue

--- pages/api/v1/auth/verify-email.js
+++ pages/api/v1/auth/verify-email.js
@@ -6,7 +6,7 @@ import {
 
 async function handler(req, res) {
   try {
-    if (req.method === 'POST') {
+    if (req.method === 'GET') {
       return res.status(200).json(await verifyEmail(req, res));
     }
     res.status(405).json(ResponseHelper.error(405, 'Method Not Allowed'));
--- pages/verify-email.js
+++ pages/verify-email.js
@@ -1,48 +1,29 @@
-// pages/verify-email.js
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useFetch } from '@/app/redux/api';
+import useFetch from '@/app/utils/hooks/useFetch';
 
 export default function VerifyEmailSuccessPage() {
   const router = useRouter();
-  const { verifyEmail } = useFetch();
-  const [verificationMessage, setVerificationMessage] = useState('');
   const { token } = router.query;
+  const [verificationMessage, setVerificationMessage] = useState('');
+  const API_URL = token ? `/api/v1/auth/verify-email?token=${token}` : null;
+  const { data, loading, error } = useFetch(API_URL);
 
   useEffect(() => {
-    const verifyEmailToken = async () => {
-      if (token) {
-        try {
-          const data = await verifyEmail(token);
-          if (data && data.success) {
-            setVerificationMessage(
-              data.message || 'Email verified successfully',
-            );
-          } else {
-            setVerificationMessage(
-              data.message ||
-                'Invalid token or email verification link has expired.',
-            );
-          }
-        } catch (error) {
-          // Error handling
-          console.error('Email verification failed', error);
-          setVerificationMessage(
-            'Invalid token or email verification link has expired.',
-          );
-        }
-      } else {
-        console.error('Token not provided');
-        setVerificationMessage('Token not provided');
-      }
-    };
-
-    verifyEmailToken();
-  }, [token]); // Only trigger useEffect when token changes
+    if (error) {
+      console.error('Email verification failed', error);
+      setVerificationMessage('Invalid token or email verification link has expired.');
+    } else if (data && data.success) {
+      setVerificationMessage(data.message || 'Email verified successfully');
+    } else if (data) {
+      setVerificationMessage(data.message || 'Invalid token or email verification link has expired.');
+    }
+  }, [data, error]);
 
   return (
     <div>
-      <p>{verificationMessage}</p>
+      {loading && <p>Loading...</p>}
+      {verificationMessage && <p>{verificationMessage}</p>}
     </div>
   );
 }
