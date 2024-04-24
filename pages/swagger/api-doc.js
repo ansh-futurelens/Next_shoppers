9f81330613dc15f00952150b943f298ab3e46ea3
ansh-futurelens <futurelensdeveloper8@gmail.com>
2024-04-24 12:26:27 +0530

    SP-3:Fixed swagger build errors

--- pages/swagger/api-doc.js
+++ pages/swagger/api-doc.js
@@ -1,6 +1,9 @@
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic'; // Import dynamic from Next.js
 import swaggerJson from '../../swagger.json';
+
+// Dynamically import SwaggerUI component
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
 
 function ApiDoc() {
   return <SwaggerUI spec={swaggerJson} />;
